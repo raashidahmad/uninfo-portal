d56f7f0c6030907499f10ec2512251809df65faa
Rashid Ahmad <raashid.ahmad@gmail.com>
2021-07-12 15:29:05 +0500

    Tweaks on health first component, added vaccination component

--- src/components/health/Vaccination.js
+++ src/components/health/Vaccination.js
@@ -1,4 +1,13 @@
-function Vaccination() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
+function Vaccination() {
+    return (
+        <div className="row success-container margin-top-10">
+            <span className="row margin-left-10">
+                <h5>People Accessing Vaccination, Maternal Health & Nutrition Programmes.</h5>
+            </span>
+        </div>
+    );
 }
 export default Vaccination;
